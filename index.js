db1062b04975f223b6bdc7ae17765f66830e804a
Himank101 <b.himank101@gmail.com>
2024-09-15 16:29:18 +0530

    write to gsheet and db query

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+const SCOPES = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.file",
+];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = path.join(process.cwd(), "token.json");
 const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+const connection = require("./db");
 
-/**
+// Example query to fetch data
+connection.query("SELECT * FROM users", (err, results, fields) => {
+  if (err) {
+    console.error("Error executing query:", err.stack);
+    return;
+  }
+  console.log("Query results:", results);
+});
+
+// Close the connection when done
+connection.end(); /**
  * Reads previously authorized credentials from the save file.
  *
  * @return {Promise<OAuth2Client|null>}
@@ -70,22 +84,49 @@ async function authorize() {
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-async function listMajors(auth) {
-  const sheets = google.sheets({ version: "v4", auth });
+async function listAll(spreadsheetId, sheets, range) {
   const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: "1e5F-Nj7V6k2gUidvqJPmQ7uQUMuD4epgk-NZOjsIDGk",
-    range: "A1:B2",
+    spreadsheetId,
+    range,
   });
   const rows = res.data.values;
   if (!rows || rows.length === 0) {
     console.log("No data found.");
     return;
   }
-  console.log("Name, Colour");
   rows.forEach((row) => {
     // Print columns A and E, which correspond to indices 0 and 4.
-    console.log(`${row[0]}, ${row[1]}`);
+    console.log(row);
   });
 }
-
-authorize().then(listMajors).catch(console.error);
+async function writeSheet(spreadsheetId, sheets, range) {
+  let values = [
+    ["himank", "red", "18", "builder"],
+    ["a", "purple", "19", "b"],
+  ];
+  const resource = {
+    values,
+  };
+  try {
+    const result = await sheets.spreadsheets.values.update({
+      spreadsheetId,
+      range,
+      valueInputOption: "RAW",
+      resource,
+    });
+    console.log("%d cells updated.", result.data.updatedCells);
+    return result;
+  } catch (err) {
+    // TODO (Developer) - Handle exception
+    throw err;
+  }
+}
+async function main() {
+  const auth = await authorize();
+  const sheets = google.sheets({ version: "v4", auth });
+  const spreadsheetId = "1e5F-Nj7V6k2gUidvqJPmQ7uQUMuD4epgk-NZOjsIDGk";
+  const range = "A1:Z1000";
+  await listAll(spreadsheetId, sheets, range);
+  await writeSheet(spreadsheetId, sheets, range);
+}
+main().catch(console.error());
